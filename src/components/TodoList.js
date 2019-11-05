b7c04c3fef98a3beb8b8190ceed1351dbb0a0e44
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-05 09:29:36 -0800

    Create handle change function. Add to onChange in function.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,9 +2,13 @@ import React, { useState , useReducer} from 'react';
 import { initialState , reducer } from '../reducers/todo';
 
 const TodoList = () => {
-    const [newTodo, setNewToDo] = useState();
+    const [newTodo, setNewToDo] = useState('');
 
     const [state, dispatch] = useReducer(reducer, initialState)
+
+    const handleChange = event => {
+        setNewToDo(event.target.value)
+    }
     return (
         <div>
             <h1>Get it Done!</h1>
@@ -13,6 +17,7 @@ const TodoList = () => {
                 name = "todo"
                 id = "todo"
                 placeholder = "add todo"
+                onChange={handleChange}
                 />
                 <button>submit</button>
             </form>
