1b532a9769b360c9e61ffe1a23ea21f2614ba920
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-05 17:51:27 -0800

    Add Path component.

---
+++ src/components/Path.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Path = () => {
+    return (
+        <div>
+            <a href="https://github.com/J2Macwilliams/reducer-todo/pull/1">
+                <button>Code</button>
+            </a>
+            <a href="https://j2macwilliams.github.io/My-Portfolio/">
+                <button>Portfolio</button>
+            </a>
+
+        </div>
+    )
+}
+
+export default Path
